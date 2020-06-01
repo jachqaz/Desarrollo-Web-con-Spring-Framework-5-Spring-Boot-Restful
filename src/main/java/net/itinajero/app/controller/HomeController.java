2969dad9fab718224b5fbf9b259d0928f592ba98
Johan Casttano <jach_1997@hotmail.com>
2020-05-31 21:25:54 -0500

    016 Agregar objetos al modelo (Model)
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,6 +1,7 @@
 package net.itinajero.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +11,17 @@ public class HomeController {
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String goHome() {
         return "home";
+    }
+
+    @RequestMapping(value = "/detail")
+    public String mostrarDetalle(Model model) {
+        String tituloPelicula = "Rapidos y furiosos";
+        int duracion = 136;
+        double precioEntrada = 50;
+        model.addAttribute("titutlo", tituloPelicula);
+        model.addAttribute("duracion", duracion);
+        model.addAttribute("precio", precioEntrada);
+        return "detalle";
     }
 
 }
