409d19b5aa9bb2531a1dd89efa6ad0df52782a5f
Johan Casttano <jach_1997@hotmail.com>
2020-05-31 22:04:05 -0500

    018 JSTL cforEach - Desplegar una lista ListString
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

--- out/artifacts/cineapp/WEB-INF/views/home.jsp
+++ out/artifacts/cineapp/WEB-INF/views/home.jsp
@@ -1,12 +1,18 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
          pageEncoding="ISO-8859-1" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <title>Pagina Principal</title>
+    <title>Bienvenido a Cineapp</title>
 </head>
 <body>
-<h1>Bienvenido a la pagina Principal</h1>
+<h1>Lista de Peliculas</h1>
+<ul>
+    <c:forEach items="${peliculas}" var="pelicula">
+        <li>${pelicula}</li>
+    </c:forEach>
+</ul>
 </body>
 </html>
--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -5,11 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String goHome() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String mostrarPrincipal(Model model) {
+        List<String> peliculas = new LinkedList<>();
+        peliculas.add("Rapido y furioso");
+        peliculas.add("El aro 2");
+        peliculas.add("Aliens");
+        model.addAttribute("peliculas", peliculas);
         return "home";
     }
 
