d74ae4276126ff3f8b3407da1eb154952f4bc72f
Johan Casttano <jach_1997@hotmail.com>
2020-06-07 18:45:23 -0500

    036 Anotacion RequestParam - Ejemplo Peticion HTTP GET
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -3,9 +3,9 @@ package net.itinajero.app.controller;
 import net.itinajero.app.model.Pelicula;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -35,9 +35,10 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(value = "/detail/{id}/{fecha}", method = RequestMethod.GET)
-    public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula, @PathVariable String fecha) {
-        System.out.println("Buscando Horrios para la pelicula" + idPelicula);
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+//    public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula, @PathVariable String fecha) {
+    public String mostrarDetalle(Model model, @RequestParam("idMovie") int idPelicula, @RequestParam String fecha) {
+        System.out.println("Buscando Horarios para la pelicula" + idPelicula);
 //        System.out.println("Para la fecha" + fecha);
 //        String tituloPelicula = "Rapidos y furiosos";
 //        int duracion = 136;
