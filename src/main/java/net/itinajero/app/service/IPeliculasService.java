c8aee9c1855d66ce800ab57f086831d9a0580846
Johan Casttano <jach_1997@hotmail.com>
2020-06-08 00:38:44 -0500

    050 Clase de Servicio PeliculasServiceImpl
    Signed-off-by: Johan Casttano <jach_1997@hotmail.com>

---
+++ src/main/java/net/itinajero/app/service/IPeliculasService.java
@@ -0,0 +1,9 @@
+package net.itinajero.app.service;
+
+import net.itinajero.app.model.Pelicula;
+
+import java.util.List;
+
+public interface IPeliculasService {
+    List<Pelicula> buscarTodas();
+}
